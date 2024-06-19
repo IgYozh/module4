8d1aef94d5ed48140cafc17f517fc07fe5ea7b7d
IgYozh <yozhmake@gmail.com>
2024-06-19 18:03:16 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+
+def divide(first, second):
+    result = 0
+    if second == 0:
+        result = "Ошибка"
+        return result
+    else:
+        result = first/second
+        return result
+
+
